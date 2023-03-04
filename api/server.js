dfef68fc340eb724e9a26380d4a493e1d6fa6f2e
letienhao <85797342+letienhao@users.noreply.github.com>
2023-03-04 22:23:11 +0700

    add josn server vercel

--- api/server.js
+++ api/server.js
@@ -6,12 +6,34 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+    res.jsonp(req.query)
+  })
+  
+  // To handle POST, PUT and PATCH you need to use a body-parser
+  // You can use the one used by JSON Server
+  server.use(jsonServer.bodyParser)
+  server.use((req, res, next) => {
+    if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
+      if (['POST'].includes(req.method)) {
+        req.body.createdAt = Date.now()
+      }
+      else if (['PUT'].includes(req.method)) {
+        req.body.updatedAt = Date.now()
+      } if (new Date(req.body.publishDate).getTime() < new Date().getTime()) {
+        return res.status(422).send({
+          error: {
+            publishDate: 'Không được published vào ngày nhỏ hơn ngày hiện tại'
+          }
+        })
+      }
+    }
+    next()
+  })
+  
 server.use(router)
-server.listen(3000, () => {
+server.listen(4000, () => {
     console.log('JSON Server is running')
 })
 
